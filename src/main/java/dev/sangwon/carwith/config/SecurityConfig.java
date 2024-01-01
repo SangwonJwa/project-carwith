e4addc70a40b17f6b072645834d6e424dbcdde7a
Sangwon Jwa <jwa4610@gmail.com>
2024-01-02 04:51:46 +0900

    fix: import 제거

--- src/main/java/dev/sangwon/carwith/config/SecurityConfig.java
+++ src/main/java/dev/sangwon/carwith/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import dev.sangwon.carwith.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
