a7ddaebafacd4a63045f21f084ad2e77b0c7b3d3
Sangwon Jwa <jwa4610@gmail.com>
2024-01-11 23:42:20 +0900

    feat: 로그인 기능 구현중

--- src/main/java/dev/sangwon/carwith/user/controller/UserController.java
+++ src/main/java/dev/sangwon/carwith/user/controller/UserController.java
@@ -1,10 +1,15 @@
 package dev.sangwon.carwith.user.controller;
 
 import dev.sangwon.carwith.user.domain.User;
-import dev.sangwon.carwith.user.dto.SignUpRequest;
+import dev.sangwon.carwith.user.dto.SignupRequest;
 import dev.sangwon.carwith.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +24,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public ResponseEntity signup(@RequestBody SignUpRequest signUpRequest){
-        User user = userService.signup(signUpRequest);
+    public ResponseEntity signup(@RequestBody SignupRequest signupRequest){
+        User user = userService.signup(signupRequest);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -29,5 +34,11 @@ public class UserController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request,response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
--- src/main/java/dev/sangwon/carwith/user/service/UserService.java
+++ src/main/java/dev/sangwon/carwith/user/service/UserService.java
@@ -1,7 +1,7 @@
 package dev.sangwon.carwith.user.service;
 
 import dev.sangwon.carwith.user.domain.User;
-import dev.sangwon.carwith.user.dto.SignUpRequest;
+import dev.sangwon.carwith.user.dto.SignupRequest;
 import dev.sangwon.carwith.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +13,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User signup(SignUpRequest dto){
+    public User signup(SignupRequest dto){
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))
